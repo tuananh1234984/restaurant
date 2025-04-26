e7d0b45e37bb84be70e277a5190a069fe0268a59
tuananh1234984 <tuananhnb439@gmail.com>
2025-04-26 15:43:36 +0700

    feat:Header1

---
+++ restaurent-app-frontend/src/componens/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        <title>Danh sách nhân viên | Quản trị admin</title>
+        <body onLoad="time()" className="app sidebar-mini rtl">
+            <header className="app-header">
+                <a className="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hidden"></a>
+                <ul className="app-nav">
+                    <li><a className="app-nav__item" href="index.html"><i className="bx bx-log-out bx-rotate-180"></i></a></li>
+                </ul>
+            </header>
+        </body>
+        </>
+    );
+};
+
+export default Header;
